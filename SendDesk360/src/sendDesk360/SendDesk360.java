58854c590c19d4f44dc363a4dc09b351d469313d
Jason Coawette <jason.coawette@gmail.com>
2024-09-29 10:58:38 -0700

    Primary Button Component

--- SendDesk360/src/sendDesk360/SendDesk360.java
+++ SendDesk360/src/sendDesk360/SendDesk360.java
@@ -1,33 +1,29 @@
 package sendDesk360;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
- 
+
 public class SendDesk360 extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        // Set up a layout
+        VBox layout = new VBox();
+
+        // Create a scene
+        Scene scene = new Scene(layout, 400, 300);
+
+        // Set the scene to the stage
+        stage.setScene(scene);
+        stage.setTitle("JavaFX WebView HTML Example");
+        stage.show();
+    }
+
     public static void main(String[] args) {
         launch(args);
-    }
-    
-    public void start(Stage primaryStage) {
-    	System.out.println("ASU Hello World!");
-    	System.out.println("It started!");
-        primaryStage.setTitle("ASU Hello World Spring 2024");
-        Button btn = new Button();
-        btn.setText("Display: 'ASU says: Hello World!'");
-        btn.setOnAction(new EventHandler<>() {
-            public void handle(ActionEvent event) {
-                System.out.println("ASU: Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
     }
 }
